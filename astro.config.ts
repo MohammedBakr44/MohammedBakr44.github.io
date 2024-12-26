d7687f5fba8dbbe62b8a19ef4e7ad6be898518d7
Mohamed Bakr <mohammed.mobile.tricks@gmail.com>
2024-12-26 13:06:32 +0200

    (fix) deployment error

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   base: '/blog',
+  output: 'static'
   integrations: [
     tailwind({
       applyBaseStyles: false,
